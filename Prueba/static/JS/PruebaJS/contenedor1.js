ddc841ac74c3a1ca4cd791879e80f5094e788301
Matublaq <mati.blaquier@gmail.com>
2024-05-01 11:18:38 +0100

    none

--- Prueba/static/JS/PruebaJS/contenedor1.js
+++ Prueba/static/JS/PruebaJS/contenedor1.js
@@ -1,7 +1,7 @@
 /*
 var nombre = prompt("Introduce su nombre: ");
 var apellido = prompt("Introduce su apellido: ");
-var edad = parseInt(prompt("Introduce su edad: ")); //parseInt() es para que se interprete cono un entero y no un string lo que ingresemos en el prompt
+var edad = parseInt(prompt("Introduce su edad: ")); //parseInt() interpreta cono un entero y no un string lo que ingresemos en el prompt
 */
 document.write("<p>")
 document.write(nombre + " " + apellido);
